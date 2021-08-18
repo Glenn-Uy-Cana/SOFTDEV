fc52e654ffe37486e33c8fd76eecd9018921569f
Glenn-Uy-Cana <85110363+Glenn-Uy-Cana@users.noreply.github.com>
2021-08-18 10:40:50 +0800

    Add files via upload

---
+++ src/com/company/Teacher.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Teacher {
+    private String employee_name;
+    private int employee_id;
+    private String department;
+
+    public String getEmployee_name(){
+        return this.employee_name;
+    }
+
+    public void setEmployee_name(String employee_name){
+        this.employee_name = employee_name;
+    }
+
+    public int getEmployee_id(){
+        return this.employee_id;
+    }
+
+    public void setEmployee_name(int employee_id){
+        this.employee_id = employee_id;
+    }
+
+    public String getDepartment(){
+        return this.department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+
+
+
+}
